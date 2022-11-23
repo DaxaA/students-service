ba4567c43897ee009a94cbda3f6e071dac7386dc
DaxaA <stud.arhpva@gmail.com>
2022-11-23 11:57:46 +0400

    creating tables

--- src/main/resources/db/migration/V0001__create_tables.sql
+++ src/main/resources/db/migration/V0001__create_tables.sql
@@ -1,12 +1,12 @@
 CREATE TABLE "students" (
-                            "id"	INTEGER,
-                            "name"	TEXT NOT NULL,
-                            "phone"	TEXT NOT NULL,
-                            "address"	INTEGER NOT NULL,
-                            "year"	INTEGER NOT NULL,
-                            "specialty_id"	INTEGER NOT NULL,
-                            FOREIGN KEY("specialty_id") REFERENCES "specialties"("id"),
-                            PRIMARY KEY("id" AUTOINCREMENT)
+    "id"	INTEGER,
+    "name"	TEXT NOT NULL,
+    "phone"	TEXT NOT NULL,
+    "address"	INTEGER NOT NULL,
+    "year"	INTEGER NOT NULL,
+    "specialty_id"	INTEGER NOT NULL,
+    FOREIGN KEY("specialty_id") REFERENCES "specialties"("id"),
+    PRIMARY KEY("id" AUTOINCREMENT)
 );
 CREATE TABLE "faculties" (
     "id"	INTEGER,
