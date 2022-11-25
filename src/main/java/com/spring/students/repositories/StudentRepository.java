0b467a15eb4a274ad683a5f8ae1d7ac2bfaad862
DaxaA <stud.arhpva@gmail.com>
2022-11-25 13:10:42 +0400

    removed unused imports

--- src/main/java/com/spring/students/repositories/StudentRepository.java
+++ src/main/java/com/spring/students/repositories/StudentRepository.java
@@ -1,10 +1,8 @@
 package com.spring.students.repositories;
 
-import com.spring.students.dto.SpecialtyDTO;
 import com.spring.students.entity.Specialty;
 import com.spring.students.entity.Student;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
