4cfd56b60a52b5959a58bc01eedcfeb1861710db
DaxaA <stud.arhpva@gmail.com>
2022-11-25 13:12:56 +0400

    created controllers

---
+++ src/main/java/com/spring/students/controllers/StudentController.java
@@ -0,0 +1,48 @@
+package com.spring.students.controllers;
+
+import com.spring.students.dto.student.StudentCreateDTO;
+import com.spring.students.dto.student.StudentDTO;
+import com.spring.students.services.StudentService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/students")
+@RequiredArgsConstructor
+public class StudentController {
+
+    private final StudentService studentService;
+
+    @GetMapping
+    public List<StudentDTO> getAll() {
+        return studentService.getStudents();
+    }
+
+    @PostMapping("/new")
+    public StudentDTO addStudent(@RequestBody StudentCreateDTO studentCreateDTO) {
+        return studentService.saveStudent(studentCreateDTO);
+    }
+
+    @GetMapping("/{id}")
+    public StudentDTO getStudentById(@PathVariable Long id) {
+        return studentService.getStudentById(id);
+    }
+
+    @PutMapping("/update/{id}")
+    public StudentDTO updateStudent(@PathVariable Long id,
+                                    @RequestBody StudentCreateDTO studentCreateDTO) {
+        return studentService.updateStudent(id, studentCreateDTO);
+    }
+
+    @DeleteMapping("/delete/{id}")
+    public String deleteStudent(@PathVariable Long id) {
+        return studentService.deleteStudentById(id);
+    }
+
+    @GetMapping("/{specialty}")
+    public List<StudentDTO> getStudentsBySpecialty(@PathVariable String specialty) {
+        return studentService.getStudentsBySpecialty(specialty);
+    }
+}
